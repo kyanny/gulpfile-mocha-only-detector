5ff874191b88468772817be217baf40d3fc427a0
Kensuke Nagae <kyanny@gmail.com>
2015-08-22 00:28:46 +0900

    Fix; need to call cb function even nothing to do in the task to proceed task chain

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,32 @@ gulp.task('build', function() {
 })
 
 gulp.task('test:mocha-only-detector', function(cb) {
-  if (!process.env.CI) {
-    return;
+  if (process.env.CI) {
+    var child = spawn('./node_modules/.bin/mocha-only-detector', ['test.js'], {
+      cwd: process.cwd()
+    });
+    var stdout = '';
+    var stderr = '';
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', function(data) {
+      stdout += data;
+      gutil.log(data);
+    });
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function(data) {
+      stderr += data;
+      gutil.log(gutil.colors.red(data));;
+    });
+    child.on('close', function(code) {
+      if (code === 0) {
+        cb();
+      } else {
+        cb(stderr);
+      }
+    });
+  } else {
+    cb();
   }
-
-  var child = spawn('./node_modules/.bin/mocha-only-detector', ['test.js'], {
-    cwd: process.cwd()
-  });
-  var stdout = '';
-  var stderr = '';
-  child.stdout.setEncoding('utf8');
-  child.stdout.on('data', function(data) {
-    stdout += data;
-    gutil.log(data);
-  });
-  child.stderr.setEncoding('utf8');
-  child.stderr.on('data', function(data) {
-    stderr += data;
-    gutil.log(gutil.colors.red(data));;
-  });
-  child.on('close', function(code) {
-    if (code === 0) {
-      return cb();
-    } else {
-      return cb(stderr);
-    }
-  });
 });
 
 gulp.task('test:unit', function() {
@@ -42,5 +42,5 @@ gulp.task('test:unit', function() {
 });
 
 gulp.task('test', function() {
-  runSequence(['build'], ['test:mocha-only-detector'], ['test:unit']);
+  runSequence('build', 'test:mocha-only-detector', 'test:unit');
 });
